20191ef3075c33e43973127c7cb8e39ffd9e3558
fdossantos95 <fdossantos95@hotmail.com>
2021-01-05 21:57:35 -0300

    Concluida

--- window.py
+++ window.py
@@ -3,5 +3,15 @@ import PySimpleGUI as sg
 sg.theme('DarkAmber')
 
 layout = [ [sg.Text('Some text on Row 1')],
-           [],
-           []]
+           [sg.Text('Enter something on Row 2'), sg.InputText()],
+           [sg.Button('OK'), sg.Button('CANCEL')]]
+
+window = sg.Window('Window title', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'CANCEL':
+        break
+    print('You entered', values[0])
+
+window.close()
